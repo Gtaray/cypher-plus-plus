253b2267447d306e269438a2493a9d2e4ab5db7a
George Taray <gtaray@hotmail.com>
2023-03-28 11:28:25 -0700

    Cyphers added to a char sheet or parcel now auto-roll their level. Hasn't been tested with roll tables.

--- scripts/manager_item_cpp.lua
+++ scripts/manager_item_cpp.lua
@@ -1,23 +1,31 @@
 function onInit()
-	-- This doesn't evne work, because the event that's raised doesn't include the 
-	-- data for the item that was just added to a parcel. Only the parcel itself.
-	--ItemManager.registerPostTransferHandler(onItemTransfer);
+	ItemManager.registerCleanupTransferHandler(onItemTransfer);
 end
 
-function onDesktopInit()
-end
-
-function onItemTransfer(rSource, rTarget)
-	-- Handle automatically rolling levels for cyphers
-	if rSource.sType == "item" and rTarget.sType == "treasureparcel" then
-		local bCypher = DB.getValue(rSource.node, "type", "") == "cypher";
-		local sLevelRoll = DB.getValue(rSource.node, "levelroll", "");
-		local nLevel = DB.getValue(rSource.node, "level", 0);
-
-		-- Only roll for cyphers with a valid level roll string and a level that's not already set
-		if bCypher and StringManager.isDiceString(sLevelRoll) and nLevel == 0 then
-			nLevel = StringManager.evalDiceString(sLevelRoll, true);
-			--DB.setValue(rSource.node, "level", "number", nLevel);
-		end
+function onItemTransfer(rSource, rTemp, rTarget)
+	if not ItemManagerCPP.isItemCypher(rTemp.node) then
+		return;
 	end
+	-- Handle automatically rolling levels for cyphers
+	if rSource.sType == "item" and (rTarget.sType == "treasureparcel" or rTarget.sType == "charsheet") then
+		ItemManagerCPP.generateCypherLevel(rTemp.node);
+	end
+end
+
+function isItemCypher(itemNode)
+	return DB.getValue(itemNode, "type", "") == "cypher";
+end
+
+function generateCypherLevel(itemNode)
+	local sLevelRoll = DB.getValue(itemNode, "levelroll", "");
+	local nLevel = DB.getValue(itemNode, "level", 0);
+
+	if StringManager.isDiceString(sLevelRoll) and nLevel == 0 then
+		nLevel = StringManager.evalDiceString(sLevelRoll, true);
+		DB.setValue(itemNode, "level", "number", nLevel);
+
+		return nLevel;
+	end
+
+	return 0;
 end
